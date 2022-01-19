824e44f7e1783315b132f9cda9ed7d238e1832c6
Nurujjaman Pollob <nurujjamanpollob@gmail.com>
2022-01-19 14:30:37 +0600

    initial

--- app/src/main/java/dev/nurujjamanpollob/uicollectiontest/MainActivity.java
+++ app/src/main/java/dev/nurujjamanpollob/uicollectiontest/MainActivity.java
@@ -1,27 +1,17 @@
 package dev.nurujjamanpollob.uicollectiontest;
 
-import android.os.Build;
 import android.os.Bundle;
-
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
+import android.widget.Button;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import dev.nurujjamanpollob.uicollectiontest.databinding.ActivityMainBinding;
-import dev.nurujjamanpollob.uicollection.dialogs.BottomDialogView;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import dev.nurujjamanpollob.uicollection.dialogs.BottomDialogView;
+
 public class MainActivity extends AppCompatActivity {
+
 
 
     @Override
@@ -32,45 +22,31 @@ public class MainActivity extends AppCompatActivity {
 
         FloatingActionButton fab = findViewById(R.id.fab);
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-            @Override
-            public void onClick(View view) {
-
-                BottomDialogView view1 = new BottomDialogView(MainActivity.this);
-                view1.setDialogResourceByLayout(R.layout.dialog_view);
-                view1.create();
-                view1.show();
+        fab.setOnClickListener(view -> {
 
 
 
+            BottomDialogView view1 = new BottomDialogView(MainActivity.this);
+            view1.setDialogResourceByLayout(R.layout.dialog_view);
+            view1.cancelDialogOnClickOutside(true);
+            view1.setUILoadListener(new BottomDialogView.OnDialogUiLoadListener() {
+                @Override
+                public void onUiLoaded() {
 
-              //  Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                   //     .setAction("Action", null).show();
-            }
+
+                    Button signup = view1.getViewByIdentity(R.id.sign_up, new Button(getApplicationContext()));
+
+                    // Set sign up click listener
+                    signup.setOnClickListener(v -> view1.setDialogResourceByLayout(R.layout.dialog_view_sign_up));
+
+                }
+            });
+
+
+
+
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
 }
--- njpollobuicollection/src/main/java/dev/nurujjamanpollob/uicollection/dialogs/BottomDialogView.java
+++ njpollobuicollection/src/main/java/dev/nurujjamanpollob/uicollection/dialogs/BottomDialogView.java
@@ -1,14 +1,17 @@
 package dev.nurujjamanpollob.uicollection.dialogs;
 
+
 import android.content.Context;
 import android.graphics.Color;
-import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 
+import androidx.annotation.IdRes;
+import androidx.annotation.LayoutRes;
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatDialog;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
@@ -17,6 +20,13 @@ import dev.nurujjamanpollob.uicollection.R;
 public class BottomDialogView extends AppCompatDialog {
 
     private final Context context;
+    private Boolean cancelDialogOnClickOuside = false;
+    private CoordinatorLayout rootView;
+    private boolean isShowing = false;
+    private OnDialogUiLoadListener uiLoadListener;
+    private int layoutId = 0;
+
+
 
     public BottomDialogView(@NonNull Context context) {
         super(context);
@@ -30,22 +40,115 @@ public class BottomDialogView extends AppCompatDialog {
 
     }
 
+    /**
+     * Method to set Dialog Layout View programmatically.
+     * Layout direction is from bottom on the screen,
+     * @param dialogLayoutID layout ID of dialog view that needs to be drawn on screen.
+     */
 
-    public void setDialogResourceByLayout(int dialogLayoutID){
+    public void setDialogResourceByLayout(@LayoutRes int dialogLayoutID) {
 
-        // Set content view
-        setContentView(R.layout.custom_dialog_layout);
+        this.layoutId = dialogLayoutID;
 
-        Window window = getWindow();
-        window.getDecorView().setBackgroundColor(Color.TRANSPARENT);
-        window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        // So, inflate the view and attach to CoordinatorLayout
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        CoordinatorLayout cl = findViewById(R.id.bottom_view_dialog_root_coordinatorelayout);
-        inflater.inflate(dialogLayoutID, cl, true);
+            drawDialogUI(dialogLayoutID, false);
+
 
     }
 
+    public void cancelDialogOnClickOutside(Boolean isCancelOnClickOutside) {
+
+        this.cancelDialogOnClickOuside = isCancelOnClickOutside;
+    }
+
+
+    private void cancelDialogOnClickOutside() {
+
+        rootView.setOnClickListener(v -> {
+
+            if (cancelDialogOnClickOuside) {
+                onBackPressed();
+            }
+        });
+
+
+    }
+
+
+
+    public Boolean isDialogShowing(){
+
+        return isShowing;
+    }
+
+
+
+
+
+    @NonNull
+    public <T extends View> T getViewByIdentity(@IdRes int id, T viewExpected){
+
+        return rootView.findViewById(id) != null ? rootView.findViewById(id) : viewExpected;
+
+    }
+
+    public interface OnDialogUiLoadListener{
+
+        default void onUiLoaded(){}
+        default void rootUI(CoordinatorLayout view){}
+
+    }
+
+
+    public void setUILoadListener(OnDialogUiLoadListener uiLoadListener){
+
+        this.uiLoadListener = uiLoadListener;
+
+        drawDialogUI(layoutId, isShowing());
+
+
+    }
+
+
+
+    private void drawDialogUI(int id, boolean skipExisting) {
+
+
+        if (!skipExisting) {
+            // Set content view
+            setContentView(R.layout.custom_dialog_layout);
+            Window window = getWindow();
+            window.getDecorView().setBackgroundColor(Color.TRANSPARENT);
+            window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            // So, inflate the view and attach to CoordinatorLayout
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            CoordinatorLayout cl = findViewById(R.id.bottom_view_dialog_root_coordinatorelayout);
+            rootView = findViewById(R.id.custom_dialog_layout_root);
+            inflater.inflate(id, cl, true);
+            // Set Dialog click outside listener
+            cancelDialogOnClickOutside();
+            isShowing = true;
+
+
+            show();
+
+        }
+
+
+        if (isDialogShowing()) {
+
+            if (uiLoadListener != null) {
+
+                if (rootView != null) {
+
+                    uiLoadListener.onUiLoaded();
+                    uiLoadListener.rootUI(rootView);
+                }
+
+            }
+        }
+
+
+    }
 
 
 }
